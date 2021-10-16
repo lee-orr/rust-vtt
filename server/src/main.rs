1ff99aad976ae61b3eeea804085aaa30b74da1b6
lee-orr <lee-orr@users.noreply.github.com>
2021-10-15 22:18:03 -0400

    add build for server

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+use warp::Filter;
+
+#[tokio::main]
+async fn main() {
+    // GET /hello/warp => 200 OK with body "Hello, warp!"
+    let hello = warp::path!("hello" / String)
+        .map(|name| format!("Hello, {}!", name));
+
+    warp::serve(hello)
+        .run(([0,0,0,0], 3030))
+        .await;
 }
