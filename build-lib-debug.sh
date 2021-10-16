263a3564145d17651697fd0173b10a4b6d0f2bac
lee-orr <lee-orr@users.noreply.github.com>
2021-10-15 22:32:40 -0400

    add build-lib scripts

---
+++ build-lib-debug.sh
@@ -0,0 +1,3 @@
+cp ./target/debug/client_lib.dll ./client-godot/client_lib.dll || true
+cp ./target/debug/client_lib.so ./client-godot/client_lib.so || true
+cp ./target/debug/client_lib.dyn ./client-godot/client_lib.dyn || true
---
+++ build-lib.sh
@@ -0,0 +1,3 @@
+cp ./target/release/client_lib.dll ./client-godot/client_lib.dll || true
+cp ./target/release/client_lib.so ./client-godot/client_lib.so || true
+cp ./target/release/client_lib.dyn ./client-godot/client_lib.dyn || true
