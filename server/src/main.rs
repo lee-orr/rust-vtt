68628c212106efc31fb64d8400eed681ae7cab09
lee-orr <lee-orr@users.noreply.github.com>
2021-10-24 11:50:50 -0400

    get things to compile again

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,42 +1,51 @@
 use clap::{App, Arg};
 use dirs::document_dir;
-use std::{net::{Ipv4Addr, SocketAddr, SocketAddrV4}, path::PathBuf, str::FromStr};
-use warp::Filter;
 use sled::{self, Db};
+use std::{
+    net::{Ipv4Addr, SocketAddr, SocketAddrV4},
+    path::PathBuf,
+    str::FromStr,
+};
+use warp::Filter;
 
-fn parse_arguments () -> (SocketAddr, PathBuf) {
+fn parse_arguments() -> (SocketAddr, PathBuf) {
     let matches = App::new("VTT Server")
-    .version("0.1")
-    .arg(
-        Arg::with_name("host")
-            .short("h")
-            .long("host")
-            .value_name("ADDRESS")
-            .help("Sets the bound host")
-            .takes_value(true)
-            .default_value("0.0.0.0"),
-    )
-    .arg(
-        Arg::with_name("port")
-            .short("p")
-            .long("port")
-            .value_name("PORT")
-            .help("Sets the bound port")
-            .takes_value(true)
-            .default_value("3030"),
-    )
-    .arg(
-        Arg::with_name("directory")
-            .short("d")
-            .long("dir")
-            .value_name("DIRECTORY")
-            .help("Sets the source directory")
-            .takes_value(true),
-    )
-    .get_matches();
+        .version("0.1")
+        .arg(
+            Arg::with_name("host")
+                .short("h")
+                .long("host")
+                .value_name("ADDRESS")
+                .help("Sets the bound host")
+                .takes_value(true)
+                .default_value("0.0.0.0"),
+        )
+        .arg(
+            Arg::with_name("port")
+                .short("p")
+                .long("port")
+                .value_name("PORT")
+                .help("Sets the bound port")
+                .takes_value(true)
+                .default_value("3030"),
+        )
+        .arg(
+            Arg::with_name("directory")
+                .short("d")
+                .long("dir")
+                .value_name("DIRECTORY")
+                .help("Sets the source directory")
+                .takes_value(true),
+        )
+        .get_matches();
     let host = matches.value_of("host").unwrap_or("0.0.0.0");
     let port = matches.value_of("port").unwrap_or("3030");
-    let current_directory = String::from(document_dir().unwrap_or_default().to_str().unwrap_or_default());
+    let current_directory = String::from(
+        document_dir()
+            .unwrap_or_default()
+            .to_str()
+            .unwrap_or_default(),
+    );
     let directory = matches
         .value_of("directory")
         .unwrap_or_else(|| current_directory.as_str());
@@ -45,9 +54,8 @@ fn parse_arguments () -> (SocketAddr, PathBuf) {
     println!("Port: {}", port);
     println!("Directory: {}", directory);
 
-    let host_addr = SocketAddr::from_str(format!("{}:{}", host, port).as_str()).unwrap_or_else(|_|
-        SocketAddr::V4(SocketAddrV4::new(Ipv4Addr::new(0, 0, 0, 0), 3030)),
-    );
+    let host_addr = SocketAddr::from_str(format!("{}:{}", host, port).as_str())
+        .unwrap_or_else(|_| SocketAddr::V4(SocketAddrV4::new(Ipv4Addr::new(0, 0, 0, 0), 3030)));
 
     (host_addr, PathBuf::from_str(directory).unwrap_or_default())
 }
@@ -62,9 +70,9 @@ async fn main() {
     println!("Running VTT Server");
     let (host_addr, directory) = parse_arguments();
 
-    let dbResult = setup_database(&directory);
-    
-    if let Ok(_) = dbResult {
+    let db_result = setup_database(&directory);
+
+    if let Ok(_) = db_result {
         // GET /hello/warp => 200 OK with body "Hello, warp!"
         let hello = warp::path!("hello" / String).map(|name| format!("Hello, {}!", name));
 
