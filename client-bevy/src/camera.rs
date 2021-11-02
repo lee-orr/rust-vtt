7307371425ad7c016b8d9c0c8fc5413ba1aabb07
lee-orr <lee-orr@users.noreply.github.com>
2021-11-02 17:01:40 -0400

    add camera movement controls

---
+++ client-bevy/src/camera.rs
@@ -0,0 +1,95 @@
+use std::{f64::consts::PI, ops::Mul};
+
+use bevy::{prelude::*, render::camera};
+
+pub struct CameraPlugin;
+
+impl Plugin for CameraPlugin {
+    fn build(&self, app: &mut bevy::prelude::AppBuilder) {
+        app.add_startup_system(setup.system())
+        .add_system(move_camera_focus.system())
+        .add_system(move_camera.system());
+    }
+}
+
+fn setup(mut commands: Commands) {
+    let parent = commands.spawn()
+        .insert(Transform::default())
+        .insert(GlobalTransform::default())
+        .insert(CameraFocus).id();
+
+    let camera = commands.spawn_bundle(PerspectiveCameraBundle {
+        transform: Transform::from_translation(Vec3::new(-2.0, 2.5, 5.0))
+            .looking_at(Vec3::default(), Vec3::Y),
+        ..Default::default()
+    })
+    .insert(CameraRadius { radius: 2.0 })
+    .insert(CameraHeight { height: 2.0})
+    .id();
+
+    commands.entity(parent).push_children(&[camera]);
+}
+
+pub struct CameraFocus;
+
+pub struct CameraRadius {
+    pub radius: f32
+}
+
+pub struct CameraHeight {
+    pub height: f32
+}
+
+fn move_camera_focus(mut focus_query: Query<&mut Transform, With<CameraFocus>>, keys: Res<Input<KeyCode>>, time: Res<Time>) {
+    let mut movement = Vec3::ZERO;
+    let mut rotation: f32 = 0.0;
+    if keys.pressed(KeyCode::W) {
+        movement = Vec3::new(0., 0., 1.);
+    } else if keys.pressed(KeyCode::S) {
+        movement = Vec3::new(0.,0.,-1.);
+    } else if keys.pressed(KeyCode::A) {
+        movement = Vec3::new(1., 0.,0.);
+    } else if keys.pressed(KeyCode::D) {
+        movement = Vec3::new(-1., 0., 0.);
+    }
+    if keys.pressed(KeyCode::E) {
+        rotation += 1.;
+    } else if keys.pressed(KeyCode::Q) {
+        rotation -= 1.;
+    }
+    rotation = rotation * std::f32::consts::FRAC_1_PI * time.delta_seconds();
+    movement = movement.normalize_or_zero() * time.delta_seconds();
+    for mut transform in focus_query.iter_mut() {
+        let offset = transform.local_x() * movement.x + transform.local_y() * movement.y + transform.local_z() * movement.z;
+        transform.translation += offset;
+        transform.rotate(Quat::from_rotation_y(rotation));
+    }
+}
+
+fn move_camera(mut camera_query: Query<(&mut Transform, &mut CameraRadius, &mut CameraHeight)>, keys: Res<Input<KeyCode>>, time: Res<Time>) {
+    let mut height_change:f32 = 0.;
+    let mut zoom_change: f32 = 0.;
+
+    if keys.pressed(KeyCode::LShift) {
+        height_change += 1.;
+    } else if keys.pressed(KeyCode::LControl) {
+        height_change -= 1.;
+    }
+
+    if keys.pressed(KeyCode::Z) {
+        zoom_change -= 1.;
+    } else if keys.pressed(KeyCode::X) {
+        zoom_change += 1.;
+    }
+
+    height_change = height_change * time.delta_seconds();
+    zoom_change = zoom_change * time.delta_seconds();
+
+    for (mut transform, mut radius, mut height) in camera_query.iter_mut() {
+        height.height += height_change;
+        radius.radius += zoom_change;
+
+        transform.translation = Vec3::new(0., height.height, -1. * radius.radius);
+        transform.look_at(Vec3::ZERO, Vec3::Y);
+    }
+}
--- client-bevy/src/lib.rs
+++ client-bevy/src/lib.rs
@@ -1,4 +1,5 @@
 pub mod communications;
+mod camera;
 
 use bevy::prelude::*;
 use bevy_egui::{egui, EguiContext, EguiPlugin};
@@ -14,7 +15,8 @@ pub fn run() {
     app.insert_resource(Msaa { samples: 4 })
         .add_plugins(DefaultPlugins)
         .add_plugin(EguiPlugin)
-        .add_plugin(CommunicationsPlugin);
+        .add_plugin(CommunicationsPlugin)
+        .add_plugin(camera::CameraPlugin);
     #[cfg(target_arch = "wasm32")]
     app.add_plugin(bevy_webgl2::WebGL2Plugin);
     app.add_startup_system(setup.system())
@@ -50,12 +52,6 @@ fn setup(
     // light
     commands.spawn_bundle(LightBundle {
         transform: Transform::from_translation(Vec3::new(4.0, 8.0, 4.0)),
-        ..Default::default()
-    });
-    // camera
-    commands.spawn_bundle(PerspectiveCameraBundle {
-        transform: Transform::from_translation(Vec3::new(-2.0, 2.5, 5.0))
-            .looking_at(Vec3::default(), Vec3::Y),
         ..Default::default()
     });
 }
