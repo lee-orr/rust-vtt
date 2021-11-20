45ca96d1e6d9e0ab5395dd8675109bde10dc6d2c
lee-orr <lee-orr@users.noreply.github.com>
2021-11-20 18:25:47 -0500

    start setting up sdf baker

--- client-bevy/src/sdf_renderer/sdf_baker.rs
+++ client-bevy/src/sdf_renderer/sdf_baker.rs
@@ -1,10 +1,16 @@
-use bevy::{math::Vec3, prelude::Plugin};
+use bevy::{math::Vec3, prelude::{Plugin, Res, ResMut}, render2::{RenderApp, render_resource::TextureView, renderer::RenderDevice, texture::{CachedTexture, TextureCache}}};
+use wgpu::{Extent3d, TextureDescriptor, TextureFormat, TextureUsages};
 
 pub struct SDFBakerPlugin;
 
 impl Plugin for SDFBakerPlugin {
     fn build(&self, app: &mut bevy::prelude::App) {
-        app.init_resource::<SDFBakerSettings>();
+        app
+            .init_resource::<SDFBakerSettings>()
+            .init_resource::<SDFTextures>();
+        let render_app = app
+            .sub_app(RenderApp)
+            .add_startup_system(setup_textures);
     }
 }
 
@@ -20,4 +26,34 @@ impl Default for SDFBakerSettings {
             max_depth: 4,
         }
     }
+}
+
+#[derive(Default)]
+pub struct SDFTextures {
+    pub texture: Option<CachedTexture>,
+    pub view: Option<TextureView>,
+}
+
+const LAYER_SIZE: u32 = 8;
+
+fn setup_textures(settings: Res<SDFBakerSettings>,render_device: Res<RenderDevice>, mut texture_cache: ResMut<TextureCache>, mut textures: ResMut<SDFTextures>) {
+        let texture = texture_cache.get(
+            &render_device,
+            TextureDescriptor {
+                label: Some("Baked SDF"),
+                size: Extent3d {
+                    depth_or_array_layers: LAYER_SIZE,
+                    width: LAYER_SIZE,
+                    height: LAYER_SIZE,
+                },
+                mip_level_count: settings.max_depth,
+                sample_count: 1,
+                dimension: wgpu::TextureDimension::D3,
+                format: TextureFormat::R32Float,
+                usage: TextureUsages::TEXTURE_BINDING | TextureUsages::COPY_DST,
+            }
+        );
+        let view = texture.default_view.clone();
+        textures.texture = Some(texture);
+        textures.view = Some(view);
 }
