acf0fa67b36a5efd91bdb05c7d35894c37d50fee
lee-orr <lee-orr@users.noreply.github.com>
2021-12-12 16:11:09 -0500

    add unit tests to zones

--- client-bevy/src/sdf_renderer/sdf_object_zones.rs
+++ client-bevy/src/sdf_renderer/sdf_object_zones.rs
@@ -1,10 +1,26 @@
+use core::num;
 use std::collections::HashMap;
 
-use bevy::{prelude::{Plugin, Component, Commands, Entity, Query, Res, ResMut, FromWorld}, render2::{RenderApp, render_resource::{BindGroup, BindGroupLayout}, RenderStage, renderer::RenderDevice}, math::Vec3};
+use bevy::{
+    math::Vec3,
+    prelude::{Commands, Component, Entity, FromWorld, Plugin, Query, Res, ResMut},
+    render2::{
+        render_resource::{BindGroup, BindGroupLayout},
+        renderer::RenderDevice,
+        RenderApp, RenderStage,
+    },
+};
 use crevice::std140::AsStd140;
-use wgpu::{util::BufferInitDescriptor, BufferUsages, BindGroupEntry, BindGroupDescriptor, BindGroupLayoutEntry, BindingType, BufferBindingType, ShaderStages, BindGroupLayoutDescriptor};
+use wgpu::{
+    util::BufferInitDescriptor, BindGroupDescriptor, BindGroupEntry, BindGroupLayoutDescriptor,
+    BindGroupLayoutEntry, BindingType, BufferBindingType, BufferUsages, ShaderStages,
+};
 
-use super::{sdf_operation::SDFGlobalNodeBounds, sdf_baker::{SDFBakerSettings, SDFBakerPipelineDefinitions}, sdf_origin::SDFOrigin};
+use super::{
+    sdf_baker::{SDFBakerPipelineDefinitions, SDFBakerSettings},
+    sdf_operation::SDFGlobalNodeBounds,
+    sdf_origin::SDFOrigin,
+};
 
 pub struct SDFObjectZonePlugin;
 
@@ -16,7 +32,6 @@ impl Plugin for SDFObjectZonePlugin {
             .add_system_to_stage(RenderStage::Prepare, prepare_zones);
     }
 }
-
 
 #[derive(Default, Component, Clone)]
 pub struct SDFZones {
@@ -100,42 +115,36 @@ pub struct SDFZoneDefinitions {
     pub final_object: i32,
 }
 
-fn prepare_zones(
-    mut commands: Commands,
-    query: Query<(Entity, &SDFGlobalNodeBounds)>,
-    render_device: Res<RenderDevice>,
-    settings: Res<ZoneSettings>,
-    origin: Res<SDFOrigin>,
-) {
-    let mut zones = SDFZones::default();
-    let mut objects = query.iter().collect::<Vec<_>>();
-
-    objects.sort_by(|a, b| a.0.cmp(&b.0));
-
+fn process_object_zones(
+    objects: &Vec<(Entity, &SDFGlobalNodeBounds)>,
+    size: f32,
+    zones_per_dimension: i32,
+    origin: &SDFOrigin,
+) -> (Vec<i32>, Vec<SDFZoneDefinitions>, usize, f32, Vec3, Vec3) {
     let mut zone_objects: Vec<i32> = Vec::new();
     let mut active_zones: Vec<SDFZoneDefinitions> = Vec::new();
     let mut zone_hash: HashMap<(u32, u32, u32), Vec<i32>> = HashMap::new();
-    let zone_size = Vec3::ONE * (settings.size / (settings.zones_per_dimension as f32)).abs();
+    let zone_size = Vec3::ONE * (size / (zones_per_dimension as f32)).abs();
     let zone_half_size = zone_size / 2.;
     let zone_radius = zone_half_size.length();
-    let bounds_min = origin.origin - (settings.size / 2.);
-    let effective_radius = zone_radius;
+    let bounds_min = origin.origin - (size / 2.);
+    // let effective_radius = zone_radius;
 
     for (obj, (_, bounds)) in objects.iter().enumerate() {
-        let zone_bound_radius = bounds.radius + effective_radius;
+        let zone_bound_radius = bounds.radius; // + effective_radius;
         let min_zone_bound = bounds.center - zone_bound_radius;
         let max_zone_bound = bounds.center + zone_bound_radius;
         let min_zone_bound = ((min_zone_bound - bounds_min) / zone_size).floor();
-        let max_zone_bound = ((max_zone_bound - bounds_min) / zone_size).floor();
+        let max_zone_bound = ((max_zone_bound - bounds_min) / zone_size).ceil();
         for x in (min_zone_bound.x as i32)..(max_zone_bound.x as i32) {
-            if x > settings.zones_per_dimension {
+            if x > zones_per_dimension {
                 break;
             }
             if x < 0 {
                 continue;
             }
             for y in (min_zone_bound.y as i32)..(max_zone_bound.y as i32) {
-                if y > settings.zones_per_dimension {
+                if y > zones_per_dimension {
                     break;
                 }
 
@@ -143,7 +152,7 @@ fn prepare_zones(
                     continue;
                 }
                 for z in (min_zone_bound.z as i32)..(max_zone_bound.z as i32) {
-                    if z > settings.zones_per_dimension {
+                    if z > zones_per_dimension {
                         break;
                     }
                     if z < 0 {
@@ -160,9 +169,9 @@ fn prepare_zones(
         }
     }
 
-    for x in 0..settings.zones_per_dimension {
-        for y in 0..settings.zones_per_dimension {
-            for z in 0..settings.zones_per_dimension {
+    for x in 0..zones_per_dimension {
+        for y in 0..zones_per_dimension {
+            for z in 0..zones_per_dimension {
                 let zone_objects_vec = zone_hash.get_mut(&(x as u32, y as u32, z as u32));
                 let offset = Vec3::new(x as f32, y as f32, z as f32);
                 let min = offset * zone_size + bounds_min;
@@ -194,8 +203,37 @@ fn prepare_zones(
 
     let num_zones = active_zones.len();
     if num_zones == 0 {
-        active_zones.push(SDFZoneDefinitions::default())
+        active_zones.push(SDFZoneDefinitions::default());
     }
+    (
+        zone_objects,
+        active_zones,
+        num_zones,
+        zone_radius,
+        zone_size,
+        bounds_min,
+    )
+}
+
+fn prepare_zones(
+    mut commands: Commands,
+    query: Query<(Entity, &SDFGlobalNodeBounds)>,
+    render_device: Res<RenderDevice>,
+    settings: Res<ZoneSettings>,
+    origin: Res<SDFOrigin>,
+) {
+    let mut zones = SDFZones::default();
+    let mut objects = query.iter().collect::<Vec<_>>();
+
+    objects.sort_by(|a, b| a.0.cmp(&b.0));
+
+    let (zone_objects, active_zones, num_zones, zone_radius, zone_size, bounds_min) =
+        process_object_zones(
+            &objects,
+            *&settings.size,
+            *&settings.zones_per_dimension,
+            &origin,
+        );
 
     let zone_object_buffer = render_device.create_buffer_with_data(&BufferInitDescriptor {
         label: Some("Zone Objects"),
@@ -220,8 +258,6 @@ fn prepare_zones(
         ),
         usage: BufferUsages::STORAGE,
     });
-
-    // println!("Num Zones: {}", num_zones);
 
     let num_zones = render_device.create_buffer_with_data(&BufferInitDescriptor {
         label: Some("Num Zones"),
@@ -257,4 +293,104 @@ fn prepare_zones(
     zones.zone_group = Some(bind_group);
     commands.spawn().insert(zones.clone());
     commands.insert_resource(zones);
+}
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+
+    #[test]
+    fn with_no_objects_all_zones_are_empty() {
+        let objects = Vec::new();
+        let result = process_object_zones(&objects, 10., 3, &SDFOrigin { origin: Vec3::ZERO });
+        assert_eq!(result.0.len(), 1);
+        assert_eq!(result.2, 27);
+        assert_eq!(result.1.len(), result.2);
+        for zone in result.1.iter() {
+            assert_eq!(zone.final_object, zone.first_object);
+        }
+    }
+
+    #[test]
+    fn with_one_object_only_containing_zone_has_references() {
+        let objects = vec![(
+            Entity::new(0),
+            &SDFGlobalNodeBounds {
+                center: Vec3::ZERO,
+                radius: 0.2,
+            },
+        )];
+        let result = process_object_zones(&objects, 10., 3, &SDFOrigin { origin: Vec3::ZERO });
+        assert_eq!(result.0.len(), 1);
+        assert_eq!(result.2, 27);
+        assert_eq!(result.1.len(), result.2);
+        let contain_item = [13];
+        for (i, zone) in result.1.iter().enumerate() {
+            println!("{} {} {}", i, zone.final_object, zone.first_object);
+            if contain_item.contains(&i) {
+                assert_eq!(zone.final_object, zone.first_object + 1);
+            } else {
+                assert_eq!(zone.final_object, zone.first_object);
+            }
+        }
+    }
+
+    #[test]
+    fn with_two_objects_only_containing_zone_has_references() {
+        let second_pos = SDFGlobalNodeBounds {
+            center: Vec3::new(0., 4., 0.),
+            radius: 0.2,
+        };
+        let objects = vec![
+            (
+                Entity::new(0),
+                &SDFGlobalNodeBounds {
+                    center: Vec3::ZERO,
+                    radius: 0.2,
+                },
+            ),
+            (
+                Entity::new(1),
+                &second_pos,
+            ),
+        ];
+        let result = process_object_zones(&objects, 10., 3, &SDFOrigin { origin: Vec3::ZERO });
+        assert_eq!(result.0.len(), 2);
+        assert_eq!(result.2, 27);
+        assert_eq!(result.1.len(), result.2);
+        let contain_item = [13, 16];
+        for (i, zone) in result.1.iter().enumerate() {
+            println!("{} {} {}", i, zone.final_object, zone.first_object);
+            if contain_item.contains(&i) {
+                assert_eq!(zone.final_object, zone.first_object + 1);
+            } else {
+                assert_eq!(zone.final_object, zone.first_object);
+            }
+        }
+    }
+
+    #[test]
+    fn witn_only_one_object_and_a_non_centered_origin_only_containing_zones_have_object() {
+        let objects = vec![(
+            Entity::new(0),
+            &SDFGlobalNodeBounds {
+                center: Vec3::ZERO,
+                radius: 0.2,
+            },
+        )];
+        let result = process_object_zones(&objects, 10., 3, &SDFOrigin { origin: Vec3::new(0., -4., 0.)});
+        assert_eq!(result.0.len(), 1);
+        assert_eq!(result.2, 27);
+        assert_eq!(result.1.len(), result.2);
+        let contain_item = [16];
+        for (i, zone) in result.1.iter().enumerate() {
+            println!("{} {} {}", i, zone.final_object, zone.first_object);
+            if contain_item.contains(&i) {
+                assert_eq!(zone.final_object, zone.first_object + 1);
+            } else {
+                assert_eq!(zone.final_object, zone.first_object);
+            }
+        }
+    }
 }
